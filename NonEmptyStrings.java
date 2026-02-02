c17e63519122cc5a34623d5091ffa898e1f50bbe
nagendranmahavigneshshree-commits <nagendranmahavigneshshree@gmail.com>
2026-02-02 19:46:26 +0530

    Create NonEmptyStrings.java

---
+++ NonEmptyStrings.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class NonEmptyStrings {
+    public static void main(String[] args) {
+
+        List<String> listStrings = Arrays.asList(
+            "abc", "", "bc", "efg", "abcd", "", "jkl"
+        );
+
+        listStrings.stream()
+                   .filter(s -> !s.isEmpty())
+                   .forEach(System.out::println);
+    }
+}
